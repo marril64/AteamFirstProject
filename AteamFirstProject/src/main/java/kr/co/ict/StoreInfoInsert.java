2ef99dc8871b74535bd0ad9ae20dd4ce269b9c26
shimpleRan <aksent1@naver.com>
2022-04-29 21:52:28 +0900

    Merge branch 'master' of https://github.com/marril64/AteamFirstProject
    # Conflicts: storeinfo 내용추가
    #	.gitignore
    #	AteamFirstProject/.classpath
    #	AteamFirstProject/src/main/java/kr/co/ict/ReviewrUpdate.java
    #	AteamFirstProject/src/main/java/kr/co/ict/Reviewrinsert.java
    #	AteamFirstProject/src/main/java/kr/co/ict/domain/ReviewrDAO.java
    #	AteamFirstProject/src/main/webapp/review/reviewinsertForm.jsp
    #	AteamFirstProject/src/main/webapp/review/reviewrUpdateForm.jsp

---
+++ AteamFirstProject/src/main/java/kr/co/ict/StoreInfoInsert.java
@@ -0,0 +1,49 @@
+package kr.co.ict;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.ict.domain.ReviewrDAO;
+import kr.co.ict.domain.StoreInfoDAO;
+import kr.co.ict.domain.StoreInfoVO;
+
+/**
+ * Servlet implementation class StoreInsert
+ */
+@WebServlet("/storeinfoinsert")
+public class StoreInfoInsert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StoreInfoInsert() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+				String storeName =request.getParameter("storeName");
+				String storeContent =request.getParameter("storeContent");
+				String storeTime =request.getParameter("storeTime");
+				String storeAdd = request.getParameter("storeAdd");
+				int storePhone = Integer.parseInt(request.getParameter("storePhone"));
+				String menu = request.getParameter("menu");
+		
+				
+				StoreInfoDAO dao = StoreInfoDAO.getInstance();
+				dao.storeinfoInsert(storeName, storeContent, storeTime, storeAdd, storePhone, menu);
+		        response.sendRedirect("http://localhost:8181/AteamFirstProject/getstoreinfolist");
+			
+		      
+	}
+
+}
