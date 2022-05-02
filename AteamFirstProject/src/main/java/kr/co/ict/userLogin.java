dc9479626db71552a3fb1a66da4f9b2ca5897421
DESKTOP-TCSIL1V\ict03-23 <marril64@naver.com>
2022-05-02 16:29:35 +0900

    userLogin 서블릿 생성

---
+++ AteamFirstProject/src/main/java/kr/co/ict/userLogin.java
@@ -0,0 +1,45 @@
+package kr.co.ict;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.ict.domain.ProjectUserDAO;
+
+/**
+ * Servlet implementation class userLogin
+ */
+@WebServlet("/userLogin")
+public class userLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public userLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		ProjectUserDAO dao = ProjectUserDAO.getInstance();
+		
+		if (dao.passwordCheck(id, pw)) {
+			System.out.println("로그인 되었습니다. 세션을 발급합니다.");
+		} else {
+			System.out.println("없는 아이디이거나 비밀번호가 잘못되었습니다.");
+		}
+	}
+
+}
